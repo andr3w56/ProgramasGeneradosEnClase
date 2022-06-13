84da64b0904215f3dbd91aec25c134c2aa6d6cfc
andr3w56 <andrewgonzale009@gmail.com>
2022-06-13 12:59:05 -0500

    Editando

--- variastablas.cpp
+++ variastablas.cpp
@@ -1,7 +1,13 @@
-#include <>
+#include <iostream>
 using namespace std;
 
 int main()
 {
+    for (int k=1; k =>1 && k)
+    {
+    int tabla;
+    cout << "Ingrese un Numero";
+    cin >> tabla;
 
+    }
 }
