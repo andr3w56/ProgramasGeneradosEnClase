85c66c2389ac17f0f2b5f91c97d6d2973f3d87b0
andr3w56 <58153210+andr3w56@users.noreply.github.com>
2022-06-14 15:41:47 -0500

    Terminando ultimo ejercicios con do while

--- variastablas.cpp
+++ variastablas.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    for (int k=1; k =>1 && k)
+int main() {
+int tabla;
+    do
     {
-    int tabla;
-    cout << "Ingrese un Numero";
+    cout <<  "Ingrese un numero ";
     cin >> tabla;
-
+    
+    int i = 1;
+    if (tabla > 0)
+    {
+    while (i <= 10)
+     {
+        cout << "\n" << tabla << " \033[1;31mX\033[0m " << i << " = " << (tabla * i) << endl;
+        i++;
+     }
     }
+    else
+    {
+        return 0;
+    }
+    }
+    while(tabla > 0);
 }
