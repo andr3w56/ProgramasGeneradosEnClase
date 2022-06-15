1c0f2d6c52bedf2817511edd316f1945d49e4845
andr3w56 <58153210+andr3w56@users.noreply.github.com>
2022-06-14 19:01:21 -0500

    "Identando"

--- variastablas.cpp
+++ variastablas.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 int main() {
 int tabla;
-    do
-    {
-    cout <<  "Ingrese un numero ";
-    cin >> tabla;
-    
-    int i = 1;
-    if (tabla > 0)
-    {
-    while (i <= 10)
-     {
-        cout << "\n" << tabla << " X " << i << " = " << (tabla * i) << endl;
-        i++;
-     }
-    }
-    else
-    {
-        return 0;
-    }
-    }
-    while(tabla > 0);
+	do
+	{
+	cout <<  "Ingrese un numero ";
+	cin >> tabla;
+
+	int i = 1;
+	if (tabla > 0)
+	{
+	    while (i <= 10)
+	    {
+		    cout << "\n" << tabla << " X " << i << " = " << (tabla * i) << endl;
+		    i++;
+        }
+	}
+	else
+	    {
+		    return 0;
+	    }
+	}
+	while(tabla > 0);
 }
