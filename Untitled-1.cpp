5f01af9fd1260acadf28b0e760c345659880663d
andr3w56 <andrewgonzale009@gmail.com>
2022-07-28 12:01:45 -0500

    commit to all

---
+++ Untitled-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+int main(int argc, const char** argv) {
+    int x[5]; //Declaración de un vector de enteros.
+    x[0] = 10;
+    x[2] = 20;
+    x[3] = 18; //Iinicializar el vector de manera manual.
+    x[4] = 19;
+
+    std::cout << x[0] << std::endl; //Imprime ciertos elementos del vector
+    std::cout << x[5] << std::endl;
+
+    for (int i = 0; i < 5; i++){
+        std::cout << " " << x[i] << std::endl;
+    }
+    
+    return 0;
+}
