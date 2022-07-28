116252370c8760dcae67bff5ed43adc90534c4e5
andr3w56 <andrewgonzale009@gmail.com>
2022-07-28 12:21:01 -0500

    modified

--- Declaracion.cpp
+++ Declaracion.cpp
@@ -8,7 +8,13 @@ int main(){
     edades[4] = 11; //reemplaza el 1 por el 11
 
     //imprimir todo el vector 
-    for (int k = 0; k < 5; k++)
-        std::cout << "El elemento de la posicion " << k << " es : " << edades[k] << std::endl;;
+    for (int k = 0; k < 5; k++){
+        std::cout << "El elemento de la posicion " << k << " es : " << edades[k] << std::endl;
+    }
+    // Declare un vector o string y agregue 7 nombres de ciudades. Luego imprima todo el vector
+    std::string ciudades[7] = {"Esmeraldas", "Quito", "Guayakill", "Azuay", "Manabí", "Santo Domingo", "Galápagos"};
+    for(int k = 0; k < 7; k++){
+        std::cout << " " << ciudades[k] << std::endl;
+    }
     return 0;
 }
